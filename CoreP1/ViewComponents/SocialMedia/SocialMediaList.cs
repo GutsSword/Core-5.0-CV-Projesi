519631dae37804c320a1a24ab0e74fbd504a4e72
Emirhan Akca <emirhanakkca@gmail.com>
2023-03-15 23:11:41 +0300

    Update 15.03.2023

---
+++ CoreP1/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreP1.ViewComponents.SocialMedia
+{
+    public class SocialMediaList : ViewComponent
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EFSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = socialMediaManager.GetList();
+            return View(values);
+        }
+    }
+}
